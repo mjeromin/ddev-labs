bddad18a863075bcc39b71366bcab739370c94b8
Mark Jeromin <mark.jeromin@sysfrog.net>
2018-06-12 18:10:15 +0000

    completed secrets lab

--- secrets/app.py
+++ secrets/app.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template
 import requests, json, os
 
+authtoken = open(os.environ['AUTHTOKEN'], 'r').readline().strip()
+headers = {
+    "Authorization": "token " + authtoken
+}
 app = Flask(__name__, template_folder='.')
 
 @app.route('/')
 def homepage():
 
   r = requests.get(
-        'https://api.github.com/user/repos'
+        'https://api.github.com/user/repos',
+        headers=headers
       )
   return render_template('repos.html', repos=json.loads(r.text))
 
